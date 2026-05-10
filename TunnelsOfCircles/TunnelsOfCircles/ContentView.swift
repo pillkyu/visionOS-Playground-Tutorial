e4007cf5167060ef4252bc07dabb8e59a1431d21
Remi(pillkyu Chung) <ramemaru55@naver.com>
2026-05-10 12:16:36 +0900

    first commit

---
+++ TunnelsOfCircles/TunnelsOfCircles/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TunnelsOfCircles
+//
+//  Created by 정필규 on 4/30/26.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ TunnelsOfCircles/TunnelsOfCircles/TunnelsOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelsOfCirclesApp.swift
+//  TunnelsOfCircles
+//
+//  Created by 정필규 on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct TunnelsOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
